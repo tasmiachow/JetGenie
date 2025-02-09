f3eaa13d9c84e9124cde69e037c2c36b1fbf9a04
tasmiachow <tasmiachowdhury123@gmail.com>
2025-02-08 23:17:44 -0500

    added groq and itinerary page will fix styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,21 @@
-import { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import QuizPage from "./Components/QuizPage.jsx";
 import Navbar from "./components/NavBar.jsx";
 import LandingPage from "./components/LandingPage.jsx";
 import Login from "./components/Login.jsx";
-
+import ItineraryBuilder from "./components/ItineraryBuilder.jsx"; 
 
 const App = () => {
   return (
     <Router>
-      <Navbar /> {/*  Navbar appears on every page */}
+      <Navbar /> {/* Navbar appears on every page */}
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/quiz" element={<QuizPage />} />
-        {/* <Route path="/plan-trip" element={<PlanTrip />} />
-        <Route path="/saved-trips" element={<SavedTrips />} /> */}
+        <Route path="/itinerary-builder" element={<ItineraryBuilder />} /> 
       </Routes>
     </Router>
-
   );
 };
 
